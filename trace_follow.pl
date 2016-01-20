01abb5f4f9923fc1b2e161363f802b35b9e53a26
Anthony Brummett <brummett@gmail.com>
2016-01-19 22:29:09 -0600

    simple program to trace/follow

---
+++ trace_follow.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+
+print "program is running\n";
+if ($ARGV[0]) {
+    print "Everything is working ok\n";
+} else {
+    print "Something went wrong\n";
+}
+
+print "Doing some more stuff...\n";
+
+print "exiting\n";
