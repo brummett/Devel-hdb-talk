522ad21d20a32f2db03f7a0013b2a3d1206c949c
Anthony Brummett <brummett@gmail.com>
2016-01-19 23:05:25 -0600

    Move setting the time to notify_resumed()
    So other debuggers waiting for input won't count toward program time

--- Devel/SimpleProf.pm
+++ Devel/SimpleProf.pm
@@ -8,7 +8,8 @@ use Time::HiRes;
 Devel::SimpleProf->attach();
 Devel::SimpleProf->trace(1);
 
-my($last_line, $last_time);
+my($last_line, $last_time) = (0, Time::HiRes::time);
+
 sub notify_trace {
     my $now = Time::HiRes::time();
     my($self, $location) = @_;
@@ -19,7 +20,12 @@ sub notify_trace {
         printf("Line %s took %0.3f ms\n",
                 $last_line, ($now - $last_time)*1000);
     }
-
-    ($last_line, $last_time) = ($this_line, Time::HiRes::time);
 }
-        
+
+sub notify_resumed {
+    my($self, $location) = @_;
+    $last_line = $location->line;
+    $last_time = Time::HiRes::time;
+}
+
+1;
